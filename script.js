b4aa9c18b40f64dae1ee0f6082cc861d1f2e1fd5
Rafsanjani Labib <84703665+labibraf@users.noreply.github.com>
2023-08-30 14:42:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// make animation in login page
+let signinBtn = document.getElementById("signBtn");
+let nameField = document.getElementById("nameField")
+
+signinBtn.onclick - function(){
+    nameField.style.maxHeight = "0";
+}
